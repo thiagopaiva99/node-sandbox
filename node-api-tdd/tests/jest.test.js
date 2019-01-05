023dd6344eb223036b7d81f16c9999c52332fb81
Thiago Galvani <thiagopaiva99@gmail.com>
2019-01-05 19:37:52 -0200

    Creating some tests for play with jest

--- node-api-tdd/tests/jest.test.js
+++ node-api-tdd/tests/jest.test.js
@@ -6,3 +6,12 @@ test('should know the main assertions of the jest', () => {
     expect(number).not.toBeNull();
     expect(number).toBe(10);
 });
+
+test('should work with objects', () => {
+    const user = {
+        name: 'John',
+        mail: 'john@mail.com',
+    };
+
+    expect(user).toHaveProperty('name', 'John');
+});
