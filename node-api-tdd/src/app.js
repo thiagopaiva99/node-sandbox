6a653d29e983b0b7f186359950e00deef2afdd98
Thiago Galvani <thiagopaiva99@gmail.com>
2019-01-06 02:02:40 -0200

    Addking knex logger to get the application logs on tests

--- node-api-tdd/src/app.js
+++ node-api-tdd/src/app.js
@@ -1,9 +1,12 @@
 const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
+// const knexLogger = require('knex-logger');
 const knexFile = require('../knexfile');
 
 app.db = knex(knexFile.test);
+
+// app.use(knexLogger(app.db));
 
 consign({ cwd: 'src', verbose: false })
     .include('./config/middlewares.js')
