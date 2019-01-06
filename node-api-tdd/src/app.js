bfc2f7268332c1dc2c36102638f6e3e794bcfa0a
Thiago Galvani <thiagopaiva99@gmail.com>
2019-01-06 00:47:02 -0200

    Creating the  tests and route

--- node-api-tdd/src/app.js
+++ node-api-tdd/src/app.js
@@ -4,4 +4,15 @@ app.get('/', (req, res) => {
     res.status(200).send();
 });
 
+app.get('/users', (req, res) => {
+    const users = [
+        {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            },
+        ];
+
+    res.status(200).json(users);
+});
+
 module.exports = app;
---
+++ node-api-tdd/tests/user.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+test('should return all users', () => {
+    return request(app).get('/users')
+        .then((response) => {
+            expect(response.status).toBe(200);
+            expect(response.body).toHaveLength(1);
+            expect(response.body[0]).toHaveProperty('name', 'John Doe');
+        });
+});
